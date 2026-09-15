35da2f04d45aaab4899f24565c0373087fb4ae05
pr-09 <pr7509645@gmail.com>
2026-09-15 22:20:21 +0530

    Prepare project for deployment

---
+++ Front-end/online-canteen-ui/src/app/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+
+  apiGateway: 'http://localhost:9090',
+  googleClientId: '64355227379-s2254hgh8j58cg6p48v9qr12eqe6hsr3.apps.googleusercontent.com',
+  rozarpayKeyId: 'rzp_test_TKsNh0krjimjTG',
+};
